3520f916636f1e583903e868d728b13fdcbb7b74
gabri <gabrieldextbezerradasilva@gmail.com>
2023-03-12 16:31:22 -0300

    parte 1:10:55 do video, ultima linha escrita foi a 48.

--- app/src/main/java/com/example/blocodenotas/Database/NotesAdapter.kt
+++ app/src/main/java/com/example/blocodenotas/Database/NotesAdapter.kt
@@ -1,10 +1,92 @@
 package com.example.blocodenotas.Database
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.AbsListView.RecyclerListener
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.blocodenotas.Models.Note
+import com.example.blocodenotas.R
+import kotlin.random.Random
 
-class NotesAdapter  {
+class NotesAdapter (private val context : Context, val listener: NotesClickListener) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
 
-    inner class NoteViewHolder(itemView :view) : RecyclerView.ViewHolder(itemView)
 
+    private val NotesList : ArrayList<Note>()
+    private val FullList  : ArrayList<Note>()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+       return NoteViewHolder(
+           LayoutInflater.from(context).inflate(R.layout.list_item,parent,false)
+       )
+    }
+
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = NotesList[position]
+        holder.title.text = CurrentNote.title
+        holder.title.isSelected = true
+
+        holder.note_tv.text = CurrentNote.note
+        holder.date.text = CurrentNote.date
+        holder.date.isSelected = true
+
+        holder.notes_layout.setCardBackgroundColor(holder.itemView.resources.getColor(randomColor(),null))
+
+
+        holder.notes_layout.setOnClickListener{
+
+            listener.onItemClicked(NotesList[holder.adapterPosition])
+        }
+
+        holder.notes_layout.setOnLongClickLitener{
+
+
+            listener.onItemClicked(NotesList[holder.adapterPosition])
+
+        }
+
+    }
+
+
+    override fun getItemCount(): Int {
+        return NotesList.size
+    }
+
+
+     fun randomColor() : Int{
+
+        val list = ArrayList<Int>()
+        list.add(R.color.NoteColor1)
+        list.add(R.color.NoteColor2)
+        list.add(R.color.NoteColor3)
+        list.add(R.color.NoteColor4)
+        list.add(R.color.NoteColor5)
+        list.add(R.color.NoteColor6)
+
+        val seed = System.currentTimeMillis().toInt()
+        val randomIndex = Random(seed).nextInt(list.size)
+        return list[randomIndex]
+
+
+
+    }
+
+    inner class NoteViewHolder(itemView :view) : RecyclerView.ViewHolder(itemView){
+
+        val notes_layout = itemView.findViewById<CardView>(R.id.Card_layout)
+        val title = itemView.findViewById<TextView>(R.id.tv_title)
+        val note_tv = itemView.findViewById<TextView>(R.id.tv_note)
+        val date = itemView.findViewById<TextView>(R.id.tv_date)
+
+
+    }
+
+    interface NotesClickListener{
+        fun onItemClicked (note:Note)
+        fun onLongItemClicked(note:Note,cardView: CardView)
+    }
 }
